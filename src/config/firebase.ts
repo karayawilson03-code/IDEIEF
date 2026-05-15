1cf8611e1031c4981c6e63f80569c61fd2131d7e
wilson karaya <karayawilson03@gmail.com>
2026-05-15 07:43:18 +0300

    fix: add AsyncStorage persistence to Firebase Auth

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey:"AIzaSyB7fyiKPxVQINgpp16_d__-cXthoHhunuM",
@@ -9,11 +10,13 @@ const firebaseConfig = {
   projectId:"ideief",
   storageBucket:"ideief.firebasestorage.appspot.com",
    messagingSenderId: "301800921832",
-  appId: "1:301800921832:web:787b9e1ab244851875075d",
+  appId: "1:301800921832:web:787b9e1ab244851875075d"
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 export const db = getFirestore(app);
 export const storage = getStorage(app);
